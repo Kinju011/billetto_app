962167379505ffce4b88ee312ce2188c947e0961
Kinjal Patel <kinju011@gmail.com>
2026-04-19 22:42:04 +0530

    Event model unit test

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  it "is invalid without a title" do
+    event = Event.new(title: nil)
+    expect(event).not_to be_valid
+  end
+
+  it "is invalid without a date" do
+    event = Event.new(date: nil)
+    expect(event).not_to be_valid
+  end
+end
